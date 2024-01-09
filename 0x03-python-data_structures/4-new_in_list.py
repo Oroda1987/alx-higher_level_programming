8a0b423c8a2c2191ed6314e5f0a4d46081fa88a1
Oroda1987 <orodairene4@gmail.com>
2024-01-08 17:30:02 -0800

    replaces an element

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -2,7 +2,7 @@
 def new_in_list(my_list, idx, element):
 	temp = my_list[:]
 	if idx < 0 or idx >= len(my_list):
-		return (temp)
+		return temp
 	else:
 		temp[idx] = element
-		return (temp)
+		return temp
